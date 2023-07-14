8e32da894b0bd33bace1f98bc5fe651b9e7cacc3
Abhoy Gorai <abhoygorai04@gmail.com>
2023-07-14 21:06:14 +0530

    past pass api added

--- APIS/hostel/getAllPasses.js
+++ APIS/hostel/getAllPasses.js
@@ -2,29 +2,65 @@ const ClosedPass = require("../../db/models/ClosedPass");
 
 const getPassList = async (req, res) => {
   let today = new Date();
-  console.log(today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds())
-  console.log(today)
+  today.setHours(5, 30, 0, 0);
 
   let tomorrow = new Date(today);
   tomorrow.setDate(tomorrow.getDate() + 1);
-//   console.log(tomorrow)
 
   const user = req.user;
-  const type = req.type;
+  const type = req.body.type;
+
   if (type === "today") {
+    console.log("check");
     try {
       const closedPasses = await ClosedPass.find({
         "hostelDetails.hostelName": user.hostelName,
-        // closingDate: {
-        //   $gt: today,
-        //   $lt: tomorrow,
-        // },
+        closingDate: {
+          $gt: today,
+          $lt: tomorrow,
+        },
       }).sort({ closingDate: -1 });
 
       return res.status(200).json({ size: closedPasses.length, closedPasses });
     } catch (error) {
       console.log(error);
-      res.status(500).json({ message: "Server error" });
+      return res.status(500).json({ message: "Server error" });
+    }
+  }
+
+  if (type === "yesterday") {
+    today.setDate(today.getDate() - 1);
+    tomorrow.setDate(tomorrow.getDate() - 1);
+    try {
+      const closedPasses = await ClosedPass.find({
+        "hostelDetails.hostelName": user.hostelName,
+        closingDate: {
+          $gt: today,
+          $lt: tomorrow,
+        },
+      }).sort({ closingDate: -1 });
+
+      return res.status(200).json({ size: closedPasses.length, closedPasses });
+    } catch (error) {
+      console.log;
+    }
+  }
+
+  if (type === "month") {
+    today.setDate(today.getDate() - 30);
+    tomorrow.setHours(5, 30, 0, 0);
+    try {
+      const closedPasses = await ClosedPass.find({
+        "hostelDetails.hostelName": user.hostelName,
+        closingDate: {
+          $gt: today,
+          $lt: tomorrow,
+        },
+      }).sort({ closingDate: -1 });
+
+      return res.status(200).json({ size: closedPasses.length, closedPasses });
+    } catch (error) {
+      console.log;
     }
   }
 
