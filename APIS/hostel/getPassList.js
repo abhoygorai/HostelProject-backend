7ab527c27215f5aee1a1ebc1c59a4ea5950fc03b
Abhoy Gorai <abhoygorai04@gmail.com>
2023-07-29 11:49:03 +0530

    warden data in getPassList

--- APIS/hostel/getPassList.js
+++ APIS/hostel/getPassList.js
@@ -6,12 +6,22 @@ const getPassList = async (req, res) => {
   try {
     const openPass = await OpenedPass.find({
       "hostelDetails.hostelName": user.hostelName,
-    });
-    console.log(openPass);
+    })
+      .populate({
+        path: "warden",
+        select: "name",
+      })
+      .populate({
+        path: "openedBy",
+        select: "name",
+      });
     const generatedPass = await Pass.find({
       "hostelDetails.hostelName": user.hostelName,
-    });
-
+    })
+      .populate({
+        path: "warden",
+        select: "name",
+      })
     return res.status(200).json([...generatedPass, ...openPass]);
   } catch (error) {
     console.log(error);
